245ff7a7da9090f657b028190132b242f49e40a6
Samuel Drapeau <samuel.drapeau@gmail.com>
2024-04-18 15:08:43 +0800

    all

---
+++ workshop/javascripts/mathjax.js
@@ -0,0 +1,19 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex",
+  },
+  chtml: {
+    displayAlign: "left",
+  },
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
